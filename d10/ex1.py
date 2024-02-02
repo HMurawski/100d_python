f0ed660c407d7f5e551525031150ec55f8d94125
Hubert Murawski <h.murawski9@gmail.com>
2024-02-02 20:28:49 +0100

    day 10 - Calculator

---
+++ d10/ex1.py
@@ -0,0 +1,11 @@
+
+
+
+def format_name(f_name, l_name):
+    f_f_name = f_name.title()
+    f_l_name = l_name.title()
+
+    return (f"{f_f_name} {f_l_name}")
+
+formated_string = format_name('HubErt', "MurAWSKi")
+print(formated_string)
