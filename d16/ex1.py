ab68e0344877eb2614978c610053c877199e28b9
Hubert Murawski <h.murawski9@gmail.com>
2024-02-20 21:04:30 +0100

    ending d16

---
+++ d16/ex1.py
@@ -0,0 +1,14 @@
+import turtle
+
+timmy = turtle.Turtle()
+
+my_screen = turtle.Screen()
+
+print(my_screen.canvheight)
+
+timmy.shape("turtle")
+timmy.color("pink")
+timmy.forward(100)
+print(timmy)
+
+my_screen.exitonclick()
