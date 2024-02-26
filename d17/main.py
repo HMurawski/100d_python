1543260f93cf1837000a31dafece5a4a445d8e00
Hubert Murawski <h.murawski9@gmail.com>
2024-02-26 21:04:08 +0100

    d17 - quiz brain

--- d17/main.py
+++ d17/main.py
@@ -12,6 +12,11 @@ for question in question_data:
     question_bank.append(new_question)
 
 
-
 quiz = QuizBrain(question_bank)
-quiz.next_question()
+
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You have completed the quiz")
+print(f"Your final score is: {quiz.score} / {len(question_bank)} ")
