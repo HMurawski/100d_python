e3fa6fa13918768937490edd3ceb4b2be6f6a23d
Hubert Murawski <h.murawski9@gmail.com>
2024-03-12 21:45:54 +0100

    d23, turtle&carmanager creation

--- d23/car_manager.py
+++ d23/car_manager.py
@@ -1,7 +1,19 @@
+from turtle import Turtle
+import random
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
 class CarManager:
-    pass
+    def __init(self):
+
+        all_cars = []
+
+    def create_cars(self):
+        new_car = Turtle("square")
+        new_car.shapezise(stretch_wid=2, stretch_len=1)
+        new_car.penup()
+        new_car.color(random.choice(COLORS))
+        random_y = random.randint()
+
