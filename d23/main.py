841b7cb6ed582d727f2a2231df49438b1d159c87
Hubert Murawski <h.murawski9@gmail.com>
2024-03-16 18:41:49 +0100

    added car manager, creating and passing cars

--- d23/main.py
+++ d23/main.py
@@ -9,7 +9,7 @@ screen.title("Turtle Crossing Game")
 screen.setup(width=600, height=600)
 screen.tracer(0)
 player = Player()
-
+car_manager = CarManager()
 
 screen.onkey(player.go_up, "w")
 
@@ -21,6 +21,9 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_cars()
+    car_manager.move_cars()
 
 
 
